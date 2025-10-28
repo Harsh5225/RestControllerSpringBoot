fe54ab893f37321907052d8a928328c9d5942a33
Harsh <harshsingh94070@gmail.com>
2025-10-29 00:48:26 +0530

    Initial commit - Spring Boot REST project

---
+++ Springproj01/src/main/java/com/Harsh/App.java
@@ -0,0 +1,18 @@
+package com.Harsh;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context= new ClassPathXmlApplicationContext("Beans.xml");
+        Car carr=  context.getBean(Car.class);
+        carr.drive();
+    }
+}
